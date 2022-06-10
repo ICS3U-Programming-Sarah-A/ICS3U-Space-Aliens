00124d5d2573ade5f924eedb6f6d3d3d11b37b0e
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2022-06-10 09:14:57 -0400

    Finished CPT-03

--- code.py
+++ code.py
@@ -12,25 +12,35 @@ def game_scene():
 
     # import image for the CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # set background image to 0 & the size
     # 10 x 8 tiles of the size 16x16
     background = stage.Grid(image_bank_background, 10, 8)
 
+    # the sprite will be updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
     # create a stage for the background  to show up on
-    # and set the frame rate to 0 fps
+    # and the size (10x8 tiles of the size 16x16)
     game = stage.Stage(ugame.display, 60)
     
-    # sets layer of all the spite so tha items show up
+    # sets layer of all the spite so that items show up
     # in order
-    game.layers = [background]
+    game.layers = [ship] + [background]
 
     # render all sprites
     game.render_block()
 
-    # repeat foever
+    # repeat forever
     while True:
-        pass # temp placehold
+        # get user input
+
+        # update game logic for the characters to move
+
+        # redraw the ship
+        game.render_sprites([ship])
+        game.tick()
 
 if __name__ == "__main__":
     game_scene()
