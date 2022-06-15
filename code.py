50185d8f0a781186af5d2a98a78330e71f800b71
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2022-06-15 11:51:12 -0400

    Finished day 07.

--- code.py
+++ code.py
@@ -9,6 +9,59 @@ import stage
 import constants
 
 
+def menu_scene():
+    # this function the main game scene
+
+    # import image for the CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # adds the text objects
+    text = []
+    text1 = stage.Text(
+        width=29, height=12, font=None, palette=constants.NEW_PALETTE, buffer=None
+    )
+    text1.move(20, 10)
+    text1.text("MT Game Studios")
+    text.append(text1)
+
+    text2 = stage.Text(
+        width=29, height=12, font=None, palette=constants.NEW_PALETTE, buffer=None
+    )
+    text2.move(40, 110)
+    text2.text("PRESS START")
+    text.append(text2)
+
+    # set background image to 0 & the size
+    # 10 x 8 tiles of the size 16x16
+    background = stage.Grid(
+        image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y
+    )
+
+    # create a stage for the background  to show up on
+    # and the size (10x8 tiles of the size 16x16)
+    game = stage.Stage(ugame.display, constants.FPS)
+
+    # sets layer of all the spite so that items show up
+    # in order
+    game.layers = text + [background]
+
+    # render all sprites
+    game.render_block()
+
+    # repeat forever
+    while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        # this if statement executes user
+        # input & does the following
+        if keys & ugame.K_START != 0:
+            game_scene()
+
+        # redreaws the sprite
+        game.tick()
+
+
 def game_scene():
     # this function the main game scene
 
@@ -23,21 +76,28 @@ def game_scene():
     select_button = constants.button_state["button_up"]
 
     # imports the sound into the game
-    pew_sound = open("pew.wav", 'rb')
+    pew_sound = open("pew.wav", "rb")
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
 
     # set background image to 0 & the size
     # 10 x 8 tiles of the size 16x16
-    background = stage.Grid(image_bank_background, constants.SCREEN_X, constants.SCREEN_GRID_Y)
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_X, constants.SCREEN_GRID_Y
+    )
 
     # the sprite will be updated every frame
-    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
 
-    alien = stage.Sprite(image_bank_sprites, 9,
-                        int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
-                        16)
+    alien = stage.Sprite(
+        image_bank_sprites,
+        9,
+        int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
+        16,
+    )
 
     # create a stage for the background  to show up on
     # and the size (10x8 tiles of the size 16x16)
@@ -54,7 +114,7 @@ def game_scene():
     while True:
         # get user input
         keys = ugame.buttons.get_pressed()
-        
+
         # this block of code executes when the A button is
         # pressed
         if keys & ugame.K_O != 0:
@@ -76,27 +136,29 @@ def game_scene():
             print("Start")
         if keys & ugame.K_SELECT:
             print("Select")
-       
+
         if keys & ugame.K_RIGHT != 0:
             if ship.x < (constants.SCREEN_X - constants.SPRITE_SIZE):
                 ship.move((ship.x + constants.SPRITE_MOVEMENT_SPEED), ship.y)
             else:
                 ship.move((constants.SCREEN_X - constants.SPRITE_SIZE), ship.y)
-       
+
         if keys & ugame.K_LEFT != 0:
             if ship.x > 0:
                 ship.move((ship.x - constants.SPRITE_MOVEMENT_SPEED), ship.y)
             else:
                 ship.move(0, ship.y)
-        
+
         if keys & ugame.K_UP != 0:
             pass
         if keys & ugame.K_DOWN != 0:
             pass
-        
+
         # play sounds once A button has been pressed
         if a_button == constants.button_state["button_just_pressed"]:
             sound.play(pew_sound)
+            # sound play pew sound
+            pass
 
         # redraw the ship
         game.render_sprites([ship] + [alien])
@@ -104,4 +166,4 @@ def game_scene():
 
 
 if __name__ == "__main__":
-    game_scene()
+    menu_scene()
