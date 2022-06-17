554456da76f937ca16ad15f9d4356cd6dc56bd40
SarahAndrew05 <99266857+SarahAndrew05@users.noreply.github.com>
2022-06-16 22:00:56 -0400

    Completed 08 & 09.

--- code.py
+++ code.py
@@ -3,12 +3,10 @@
 # Created by: Sarah
 # Created on: June 9th, 2022.
 # This program  is the Space Alien program on the Pybadge.
-import constants
 import random
 import time
 
-
-
+import constants
 import stage
 import ugame
 
